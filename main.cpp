d6c7af17a7cb5f3738ba4af39baf56580eb08e09
artkopt <artkopt@mail.ru>
2021-11-26 15:26:18 +0300

    Changing the program for laboratory work

--- main.cpp
+++ main.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
+#include <vector>
+#include <clocale>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    setlocale(LC_ALL, "Russian");
+
+    //Ввод данных. Задаём размер массива.
+    int n;
+    cout << "Размер массива: ";
+    cin >> n;
+    vector <int> a(n); // объявляем массив размером n
+
+    //Ввод чисел в массив в диалоге с пользователем
+    cout << "Введите свой массив чисел через пробел " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "Введите " << i + 1 << " число: ";
+        cin >> a[i];
+    }
+
+    //Выводим массив на экран
+    cout << "Ваш массив: ";
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+
+    //Вводим заданное пользователем значение
+    int check;
+    cout << "Введите заданное значение: ";
+    cin >> check;
+
+    //Обработка массива чисел
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (a[i] + a[j] == check) {
+                cout << "Ваша пара чисел: " << "(" << a[i] << " , " << a[j] << ")" << endl;
+            }
+        }
+    }
     return 0;
 }
